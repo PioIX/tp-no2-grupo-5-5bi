c7d914d28da977448cc10977e9b2b893fa01c21e
Tomás Torres <tomastorres1710@gmail.com>
2019-06-07 00:15:17 -0300

    Azar(0 al 90)
    Probando... 1 2 3 ndea
    Agregue un azar en un txt para probar algo muchachos

--- BingoPioIX/BingoPioIX/bingo.Designer.vb
+++ BingoPioIX/BingoPioIX/bingo.Designer.vb
@@ -22,16 +22,40 @@ Partial Class bingo
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnRandom = New System.Windows.Forms.Button()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'btnRandom
+        '
+        Me.btnRandom.Location = New System.Drawing.Point(638, 56)
+        Me.btnRandom.Name = "btnRandom"
+        Me.btnRandom.Size = New System.Drawing.Size(75, 23)
+        Me.btnRandom.TabIndex = 0
+        Me.btnRandom.Text = "Aleatorio"
+        Me.btnRandom.UseVisualStyleBackColor = True
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(628, 85)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumero.TabIndex = 1
         '
         'bingo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(752, 604)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.btnRandom)
         Me.Name = "bingo"
         Me.Text = "Bingo Pio IX"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents btnRandom As Button
+    Friend WithEvents txtNumero As TextBox
 End Class
