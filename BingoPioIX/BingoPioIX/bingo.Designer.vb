c0a7df63fc521b528d088c895473e8bccfb08497
MatiasCentocchi <matiascentocchi@gmail.com>
2019-06-08 11:27:32 -0300

    Segundo intento de Merge
    Hola Chicuelos, en mi anterior Merge Conflict no me daba la posibilidad de Mergearlo ni de corregir los Merge Conflicts ;) espero que ahora funcione!

--- BingoPioIX/BingoPioIX/bingo.Designer.vb
+++ BingoPioIX/BingoPioIX/bingo.Designer.vb
@@ -22,33 +22,173 @@ Partial Class bingo
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.btnRandom = New System.Windows.Forms.Button()
-        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.btnbingo = New System.Windows.Forms.Button()
+        Me.lblquiniela = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblnum6 = New System.Windows.Forms.Label()
+        Me.lblnum5 = New System.Windows.Forms.Label()
+        Me.lblnum4 = New System.Windows.Forms.Label()
+        Me.lblnum3 = New System.Windows.Forms.Label()
+        Me.lblnum2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblnum = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnsacar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'btnRandom
+        'btnbingo
         '
-        Me.btnRandom.Location = New System.Drawing.Point(638, 56)
-        Me.btnRandom.Name = "btnRandom"
-        Me.btnRandom.Size = New System.Drawing.Size(75, 23)
-        Me.btnRandom.TabIndex = 0
-        Me.btnRandom.Text = "Aleatorio"
-        Me.btnRandom.UseVisualStyleBackColor = True
+        Me.btnbingo.Location = New System.Drawing.Point(672, 545)
+        Me.btnbingo.Name = "btnbingo"
+        Me.btnbingo.Size = New System.Drawing.Size(75, 23)
+        Me.btnbingo.TabIndex = 26
+        Me.btnbingo.Text = "BINGO!"
+        Me.btnbingo.UseVisualStyleBackColor = True
         '
-        'txtNumero
+        'lblquiniela
         '
-        Me.txtNumero.Location = New System.Drawing.Point(628, 85)
-        Me.txtNumero.Name = "txtNumero"
-        Me.txtNumero.Size = New System.Drawing.Size(100, 20)
-        Me.txtNumero.TabIndex = 1
+        Me.lblquiniela.AutoSize = True
+        Me.lblquiniela.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.lblquiniela.Location = New System.Drawing.Point(622, 179)
+        Me.lblquiniela.Name = "lblquiniela"
+        Me.lblquiniela.Size = New System.Drawing.Size(0, 24)
+        Me.lblquiniela.TabIndex = 25
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label4.Location = New System.Drawing.Point(12, -2)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(101, 20)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "Super Bingo"
+        '
+        'lblnum6
+        '
+        Me.lblnum6.AutoSize = True
+        Me.lblnum6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.lblnum6.Location = New System.Drawing.Point(704, 426)
+        Me.lblnum6.Name = "lblnum6"
+        Me.lblnum6.Size = New System.Drawing.Size(16, 17)
+        Me.lblnum6.TabIndex = 23
+        Me.lblnum6.Text = "2"
+        Me.lblnum6.Visible = False
+        '
+        'lblnum5
+        '
+        Me.lblnum5.AutoSize = True
+        Me.lblnum5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!)
+        Me.lblnum5.Location = New System.Drawing.Point(700, 391)
+        Me.lblnum5.Name = "lblnum5"
+        Me.lblnum5.Size = New System.Drawing.Size(24, 25)
+        Me.lblnum5.TabIndex = 22
+        Me.lblnum5.Text = "4"
+        Me.lblnum5.Visible = False
+        '
+        'lblnum4
+        '
+        Me.lblnum4.AutoSize = True
+        Me.lblnum4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!)
+        Me.lblnum4.Location = New System.Drawing.Point(699, 349)
+        Me.lblnum4.Name = "lblnum4"
+        Me.lblnum4.Size = New System.Drawing.Size(29, 31)
+        Me.lblnum4.TabIndex = 21
+        Me.lblnum4.Text = "7"
+        Me.lblnum4.Visible = False
+        '
+        'lblnum3
+        '
+        Me.lblnum3.AutoSize = True
+        Me.lblnum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.25!)
+        Me.lblnum3.Location = New System.Drawing.Point(694, 298)
+        Me.lblnum3.Name = "lblnum3"
+        Me.lblnum3.Size = New System.Drawing.Size(39, 42)
+        Me.lblnum3.TabIndex = 20
+        Me.lblnum3.Text = "5"
+        Me.lblnum3.Visible = False
+        '
+        'lblnum2
+        '
+        Me.lblnum2.AutoSize = True
+        Me.lblnum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.25!)
+        Me.lblnum2.Location = New System.Drawing.Point(687, 239)
+        Me.lblnum2.Name = "lblnum2"
+        Me.lblnum2.Size = New System.Drawing.Size(57, 63)
+        Me.lblnum2.TabIndex = 19
+        Me.lblnum2.Text = "2"
+        Me.lblnum2.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label3.Location = New System.Drawing.Point(622, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(150, 20)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Últimos 5 números"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label2.Location = New System.Drawing.Point(631, 148)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 17)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "En la quiniela es:"
+        '
+        'lblnum
+        '
+        Me.lblnum.AutoSize = True
+        Me.lblnum.Font = New System.Drawing.Font("Microsoft Sans Serif", 65.25!)
+        Me.lblnum.Location = New System.Drawing.Point(631, 50)
+        Me.lblnum.Name = "lblnum"
+        Me.lblnum.Size = New System.Drawing.Size(138, 98)
+        Me.lblnum.TabIndex = 16
+        Me.lblnum.Text = "10"
+        Me.lblnum.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.Label1.Location = New System.Drawing.Point(622, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 24)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Salió el número:"
+        '
+        'btnsacar
+        '
+        Me.btnsacar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.btnsacar.Location = New System.Drawing.Point(657, 457)
+        Me.btnsacar.Name = "btnsacar"
+        Me.btnsacar.Size = New System.Drawing.Size(90, 82)
+        Me.btnsacar.TabIndex = 14
+        Me.btnsacar.Text = "Sacar número"
+        Me.btnsacar.UseVisualStyleBackColor = True
         '
         'bingo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(752, 604)
-        Me.Controls.Add(Me.txtNumero)
-        Me.Controls.Add(Me.btnRandom)
+        Me.ClientSize = New System.Drawing.Size(780, 604)
+        Me.Controls.Add(Me.btnbingo)
+        Me.Controls.Add(Me.lblquiniela)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblnum6)
+        Me.Controls.Add(Me.lblnum5)
+        Me.Controls.Add(Me.lblnum4)
+        Me.Controls.Add(Me.lblnum3)
+        Me.Controls.Add(Me.lblnum2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblnum)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnsacar)
         Me.Name = "bingo"
         Me.Text = "Bingo Pio IX"
         Me.ResumeLayout(False)
@@ -56,6 +196,17 @@ Partial Class bingo
 
     End Sub
 
-    Friend WithEvents btnRandom As Button
-    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents btnbingo As Button
+    Friend WithEvents lblquiniela As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblnum6 As Label
+    Friend WithEvents lblnum5 As Label
+    Friend WithEvents lblnum4 As Label
+    Friend WithEvents lblnum3 As Label
+    Friend WithEvents lblnum2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblnum As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnsacar As Button
 End Class
--- BingoPioIX/BingoPioIX/bingo.vb
+++ BingoPioIX/BingoPioIX/bingo.vb
@@ -1,15 +1,33 @@
 ﻿Public Class bingo
+    Dim botones As New List(Of Button)
+    Dim boton As New Button()
+    Dim a As Integer
+    Dim blacklist(100) As Integer
+    Dim i As Integer
+    Dim resp
+    Dim mem1, mem2, mem3, mem4, mem5 As Integer
+
+    Private Sub btnbingo_Click(sender As Object, e As EventArgs) Handles btnbingo.Click
+        resp = MsgBox("Felicidades, Alguien canto Bingo! Desean Seguir Jugando?", MsgBoxStyle.YesNo, "Jugadores")
+        If resp = vbNo Then End
+    End Sub
+
+    Dim strmem1, strmem2, strmem3, strmem4, strmem5 As String
+    Dim ronda As Integer
     Private Sub bingo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim botones As New List(Of Button)
-        Dim boton As New Button()
-        Dim a As Integer
-        For i = 0 To 9 Step 1
-            For j = 0 To 8 Step 1
+        i = 0
+        mem1 = 0
+        mem2 = 0
+        mem3 = 0
+        mem4 = 0
+        mem5 = 0
+        For j = 0 To 8 Step 1
+            For i = 0 To 9 Step 1
                 a = j & i
                 boton.Text = a + 1
-                boton.Location = New Point(i * 60, j * 60)
+                boton.Location = New Point(20 + i * 60, 20 + j * 60)
                 botones.Add(boton)
-                boton.Enabled = True
+                boton.Enabled = False
                 boton.Height = 60
                 boton.Width = 60
                 boton.Font = New Font("Comic Sans MS", 20, FontStyle.Bold)
@@ -18,10 +36,129 @@
             Next
         Next
     End Sub
-    'Azar de 0 a 90
-    Private Sub btnRandom_Click(sender As Object, e As EventArgs) Handles btnRandom.Click
-        Dim numAleatorio As New Random()
-        Dim valorAleatorio As Integer = numAleatorio.Next(0, 90)
-        txtNumero.Text = valorAleatorio
+    Private Sub btnsacar_Click(sender As Object, e As EventArgs) Handles btnsacar.Click
+        lblnum2.Text = mem1
+        lblnum3.Text = mem2
+        lblnum4.Text = mem3
+        lblnum5.Text = mem4
+        lblnum6.Text = mem5
+        Randomize()
+        Dim Azar As Integer = CInt(Int((90 * Rnd()) + 1))
+        For m = 0 To i Step 1
+            For j = 0 To i Step 1
+                Do
+                    If Azar = blacklist(j) Then
+                        Azar = CInt(Int((90 * Rnd()) + 1))
+                    End If
+                Loop Until Azar <> blacklist(m)
+            Next
+        Next
+        mem5 = mem4
+        mem4 = mem3
+        mem3 = mem2
+        mem2 = mem1
+        mem1 = Azar
+        If ronda = 0 Then lblnum.Visible = True
+        If ronda = 1 Then lblnum2.Visible = True
+        If ronda = 2 Then lblnum3.Visible = True
+        If ronda = 3 Then lblnum4.Visible = True
+        If ronda = 4 Then lblnum5.Visible = True
+        If ronda = 5 Then lblnum6.Visible = True
+        blacklist(i) = Azar
+        lblnum.Text = Azar
+        botones(Azar - 1).BackColor = Color.Red
+        i += 1
+        ronda = ronda + 1
+
+        If Azar = 1 Then lblquiniela.Text = "Agua"
+        If Azar = 2 Then lblquiniela.Text = "Niño"
+        If Azar = 3 Then lblquiniela.Text = "San Cono"
+        If Azar = 4 Then lblquiniela.Text = "La Cama"
+        If Azar = 5 Then lblquiniela.Text = "Gato"
+        If Azar = 6 Then lblquiniela.Text = "Perro"
+        If Azar = 7 Then lblquiniela.Text = "Revolver"
+        If Azar = 8 Then lblquiniela.Text = "Incendio"
+        If Azar = 9 Then lblquiniela.Text = "Arroyo"
+        If Azar = 10 Then lblquiniela.Text = "La leche"
+        If Azar = 11 Then lblquiniela.Text = "Palito"
+        If Azar = 12 Then lblquiniela.Text = "Soldado"
+        If Azar = 13 Then lblquiniela.Text = "La yeta"
+        If Azar = 14 Then lblquiniela.Text = "Borracho"
+        If Azar = 15 Then lblquiniela.Text = "Niña bonita"
+        If Azar = 16 Then lblquiniela.Text = "Anillo"
+        If Azar = 17 Then lblquiniela.Text = "Desgracia"
+        If Azar = 18 Then lblquiniela.Text = "Sangre"
+        If Azar = 19 Then lblquiniela.Text = "Pescado"
+        If Azar = 20 Then lblquiniela.Text = "La fiesta"
+        If Azar = 21 Then lblquiniela.Text = "La mujer"
+        If Azar = 22 Then lblquiniela.Text = "El loco"
+        If Azar = 23 Then lblquiniela.Text = "Mariposa"
+        If Azar = 24 Then lblquiniela.Text = "Caballo"
+        If Azar = 25 Then lblquiniela.Text = "Gallina"
+        If Azar = 26 Then lblquiniela.Text = "La misa"
+        If Azar = 27 Then lblquiniela.Text = "El peine"
+        If Azar = 28 Then lblquiniela.Text = "El cerro"
+        If Azar = 29 Then lblquiniela.Text = "San Pedro"
+        If Azar = 30 Then lblquiniela.Text = "Santa Rosa"
+        If Azar = 31 Then lblquiniela.Text = "La luz"
+        If Azar = 32 Then lblquiniela.Text = "Dinero"
+        If Azar = 33 Then lblquiniela.Text = "Cristo"
+        If Azar = 34 Then lblquiniela.Text = "Cabeza"
+        If Azar = 35 Then lblquiniela.Text = "Pajarito"
+        If Azar = 36 Then lblquiniela.Text = "Manteca"
+        If Azar = 37 Then lblquiniela.Text = "Dentista"
+        If Azar = 38 Then lblquiniela.Text = "Aceite"
+        If Azar = 39 Then lblquiniela.Text = "Lluvia"
+        If Azar = 40 Then lblquiniela.Text = "Cura"
+        If Azar = 41 Then lblquiniela.Text = "Cuchillo"
+        If Azar = 42 Then lblquiniela.Text = "Zapatilla"
+        If Azar = 43 Then lblquiniela.Text = "Balcón"
+        If Azar = 44 Then lblquiniela.Text = "La cárcel"
+        If Azar = 45 Then lblquiniela.Text = "El vino"
+        If Azar = 46 Then lblquiniela.Text = "Tomates"
+        If Azar = 47 Then lblquiniela.Text = "Muerto"
+        If Azar = 48 Then lblquiniela.Text = "Muerto habla"
+        If Azar = 49 Then lblquiniela.Text = "La carne"
+        If Azar = 50 Then lblquiniela.Text = "El pan"
+        If Azar = 51 Then lblquiniela.Text = "Serrucho"
+        If Azar = 52 Then lblquiniela.Text = "Madre"
+        If Azar = 53 Then lblquiniela.Text = "El barco"
+        If Azar = 54 Then lblquiniela.Text = "La vaca"
+        If Azar = 55 Then lblquiniela.Text = "Los gallegos"
+        If Azar = 56 Then lblquiniela.Text = "La caída"
+        If Azar = 57 Then lblquiniela.Text = "Jorabajo"
+        If Azar = 58 Then lblquiniela.Text = "Ahogado"
+        If Azar = 59 Then lblquiniela.Text = "Planta"
+        If Azar = 60 Then lblquiniela.Text = "Virgen"
+        If Azar = 61 Then lblquiniela.Text = "Escopeta"
+        If Azar = 62 Then lblquiniela.Text = "Inundacion"
+        If Azar = 63 Then lblquiniela.Text = "Casamiento"
+        If Azar = 64 Then lblquiniela.Text = "Llanto"
+        If Azar = 65 Then lblquiniela.Text = "Cazador"
+        If Azar = 66 Then lblquiniela.Text = "Lombrices"
+        If Azar = 67 Then lblquiniela.Text = "Víbora"
+        If Azar = 68 Then lblquiniela.Text = "Sobrinos"
+        If Azar = 69 Then lblquiniela.Text = "Vicios"
+        If Azar = 70 Then lblquiniela.Text = "Muerto sueño"
+        If Azar = 71 Then lblquiniela.Text = "Excrementos"
+        If Azar = 72 Then lblquiniela.Text = "Sorpresa"
+        If Azar = 73 Then lblquiniela.Text = "Hospital"
+        If Azar = 74 Then lblquiniela.Text = "Negros"
+        If Azar = 75 Then lblquiniela.Text = "Payaso"
+        If Azar = 76 Then lblquiniela.Text = "Llamas"
+        If Azar = 77 Then lblquiniela.Text = "Las piernas"
+        If Azar = 78 Then lblquiniela.Text = "Ramera"
+        If Azar = 79 Then lblquiniela.Text = "Ladrón"
+        If Azar = 80 Then lblquiniela.Text = "La bocha"
+        If Azar = 81 Then lblquiniela.Text = "Flores"
+        If Azar = 82 Then lblquiniela.Text = "Pelea"
+        If Azar = 83 Then lblquiniela.Text = "Mal tiempo"
+        If Azar = 84 Then lblquiniela.Text = "Iglesia"
+        If Azar = 85 Then lblquiniela.Text = "Linterna"
+        If Azar = 86 Then lblquiniela.Text = "Humo"
+        If Azar = 87 Then lblquiniela.Text = "Piojos"
+        If Azar = 88 Then lblquiniela.Text = "El Papa"
+        If Azar = 89 Then lblquiniela.Text = "La rata"
+        If Azar = 90 Then lblquiniela.Text = "El miedo"
     End Sub
 End Class
